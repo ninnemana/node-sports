eaf7cf9d1e8862d788d9c9ce8e7428fbbceae873
Alex Ninneman <ninnemana@gmail.com>
2012-05-14 16:43:18 -0500

    added async parallel functions to index page

--- routes/index.js
+++ routes/index.js
@@ -9,54 +9,65 @@ exports.index = function(req, res){
 };
 
 exports.category = function(req, res){
-	Category.getParents(function(catList){
-		var id = req.params.id;
-		Category.get(id,function(cat){
-
-			if(cat.sub_categories != undefined && cat.sub_categories.length > 0){
-				
-				var index = 0;
-				var subs = cat.sub_categories;
-				cat.sub_categories = new Array();
-				subs.forEach(function(sub){
-					Category.get(sub.catID,function(last){
-						index++;
-						last.sub_categories.sort(function(a, b){
-							var titleA = a.catTitle.toLowerCase(),
-								titleB = b.catTitle.toLowerCase();
-							if(titleA < titleB){
-								return -1;
+	var catList = new Array(),
+		id = req.params.id,
+		cat = new Object();
+	async.parallel([
+		function(callback){
+			Category.getParents(function(cats){
+				catList = cats;
+				callback()
+			});
+		},
+		function(callback){
+			Category.get(id,function(resp){
+				cat = resp;
+				if(cat.sub_categories != undefined && cat.sub_categories.length > 0){
+					
+					var index = 0;
+					var subs = cat.sub_categories;
+					cat.sub_categories = new Array();
+					subs.forEach(function(sub){
+						Category.get(sub.catID,function(last){
+							index++;
+							last.sub_categories.sort(function(a, b){
+								var titleA = a.catTitle.toLowerCase(),
+									titleB = b.catTitle.toLowerCase();
+								if(titleA < titleB){
+									return -1;
+								}
+								if(titleA > titleB){
+									return 1;
+								}
+								return 0;
+							});
+							cat.sub_categories.push(last);
+							cat.sub_categories.sort(function(a, b){
+								var titleA = a.catTitle.toLowerCase(),
+									titleB = b.catTitle.toLowerCase();
+								if(titleA < titleB){
+									return -1;
+								}
+								if(titleA > titleB){
+									return 1;
+								}
+								return 0;
+							});
+							
+							if(index === subs.length){
+								callback();
 							}
-							if(titleA > titleB){
-								return 1;
-							}
-							return 0;
 						});
-						cat.sub_categories.push(last);
-						cat.sub_categories.sort(function(a, b){
-							var titleA = a.catTitle.toLowerCase(),
-								titleB = b.catTitle.toLowerCase();
-							if(titleA < titleB){
-								return -1;
-							}
-							if(titleA > titleB){
-								return 1;
-							}
-							return 0;
-						});
-						
-						if(index === subs.length){
-							res.render('category', { title: cat.catTitle, locals: {categories: catList, cat: cat, selected_cat: id } });		
-						}
 					});
-				});
 
-			}else{
-				res.redirect('/category/'+cat.catID+'/parts');
-			}
-		});	
+				}else{
+					res.redirect('/category/'+cat.catID+'/parts');
+				}
+			});	
+		},
+	],function(err, callback){
+		res.render('category', { title: cat.catTitle, locals: {categories: catList, cat: cat, selected_cat: id } });
 	});
-
 };
 
 exports.category_parts = function(req, res){
